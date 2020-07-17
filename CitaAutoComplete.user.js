17fc200747544ce70826f1acb431f98f465e5d75
Andrey Luzhin <62383996+xpam-net@users.noreply.github.com>
2020-07-17 16:52:20 +0200

    Create CitaAutoComplete.user.js

---
+++ CitaAutoComplete.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Cita AutoComplete
+// @namespace    http://xpam.net/
+// @version      0.2
+// @description  AutoComplete of the cita form
+// @author       Andrey Luzhin
+// @include      https://sede.administracionespublicas.gob.es/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    'use strict';
+
+//
+
+})();
